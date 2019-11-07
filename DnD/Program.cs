04ef85b89158fe91839251d3e61dd4508dd4d9a5
lpixley1 <57221375+lpixley1@users.noreply.github.com>
2019-11-07 08:39:12 -0500

    Add files via upload

---
+++ DnD/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRoll.chooser();
+            CharacterSheet.start();
+            Console.ReadKey();
+        }
+    }
+
+    class DiceRoll
+    {
+        public static void chooser()
+        {
+            Console.WriteLine("What dice would you like to roll?");
+            Console.WriteLine("d2, d4, d6, d8, d10, d12, d20, d100, or other kind of dice");
+            string choice = Console.ReadLine();
+            if (choice == "")
+            {
+                Console.WriteLine("Please input an actual value");
+                chooser();
+            }
+            string die = new String(choice.Where(Char.IsDigit).ToArray());
+            roller(Int32.Parse(die));
+        }
+
+        public static void roller(int die)
+        {
+            Random rnd = new Random();
+            int roll = 0;
+            int total = 0;
+            int counter = 1;
+            Console.WriteLine("How many would you like to roll?");
+            string choice = Console.ReadLine();
+            string number = new String(choice.Where(char.IsDigit).ToArray());
+            int num = Int32.Parse(number);
+            while (counter <= num)
+            {
+                roll = rnd.Next(1, die + 1);
+                Console.WriteLine("Roll " + counter + " is: " + roll);
+                counter++;
+                total += roll;
+            }
+
+            Console.WriteLine("Total is: " + total);
+            Continue();
+        }
+
+        public static void Continue()
+        {
+            Console.WriteLine("Would you like to continue?");
+            string choice = Console.ReadLine();
+            choice.ToLower();
+            if (choice == "y" || choice == "yes" || choice == "")
+            {
+                Console.Clear();
+                chooser();
+            }
+            if (choice == "n" || choice == "no")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    class CharacterSheet
+    {
+        public static void start()
+        {
+            Console.WriteLine("Would you like to access a character sheet or make a new one?");
+            string choice = Console.ReadLine();
+            string Choice = new String(choice.Where(char.IsDigit).ToArray());
+            Choice.ToLower();
+            if (Choice == "new")
+            {
+                newCharacter();
+            }
+            else if (Choice == "access")
+            {
+                accessCharacter();
+            }
+            else
+            {
+                Console.WriteLine("Please input either 'new' or 'access'");
+                Console.ReadKey();
+                Console.Clear();
+                start();
+            }
+        }
+
+        public static void newCharacter()
+        {
+
+        }
+
+        public static void accessCharacter()
+        {
+
+        }
+    }
+}
